55c4bbdc63079a9ad0a643afece40fd3f6281606
alix.maynart@gmail.com <alix.maynart@gmail.com>
2023-03-07 16:10:05 +0000

    cleaning payment

--- models/staging/stg_payment_data.sql
+++ models/staging/stg_payment_data.sql
@@ -14,9 +14,9 @@ END AS payment_type
 FROM `tiller-final-project.tiller.payment_data`;
 
 
-#Checked primary key unicity and not null values.
-#Removed "dim_status" column because all values are equal to "accepted".
-#Removed "m_cashback" and "m_credit" columns because all values are equal to "0" or NUll.
-#Renamed "dim_type" column to differentiate from the one in table "order_line".
-#Casted id columns in string format.
-#Uniformized "payment_type" different values.
+--Checked primary key unicity and not null values.
+--Removed "dim_status" column because all values are equal to "accepted".
+--Removed "m_cashback" and "m_credit" columns because all values are equal to "0" or NUll.
+--Renamed "dim_type" column to differentiate from the one in table "order_line".
+--Casted id columns in string format.
+--Uniformized "payment_type" different values.
