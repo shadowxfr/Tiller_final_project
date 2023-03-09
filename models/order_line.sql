9a0022e66518af3a246dd87ff4273801637631dc
pedrofmangas@gmail.com <pedrofmangas@gmail.com>
2023-03-09 13:41:16 +0000

    ajout des colonnes id_store et store_type dans order_line

--- models/order_line.sql
+++ models/order_line.sql
@@ -1,2 +1,6 @@
-SELECT *
-FROM {{ ref('stg_order_line') }}
+SELECT l.*,
+s.id_store,
+s.store_type
+FROM {{ ref('stg_order_line') }} l
+LEFT JOIN {{ ref('order_data') }} o USING (id_order)
+LEFT JOIN {{ ref('store_data') }} s on o.id_store = s.id_store
