54746739c33c1205dbe5db2ff1c3c1a663c7c901
beltran.caroline@gmail.com <beltran.caroline@gmail.com>
2023-03-09 11:31:39 +0000

    fix yml + gestion des null sur order_services

--- models/order_per_day.sql
+++ models/order_per_day.sql
@@ -9,7 +9,7 @@ COUNT(id_order) AS nb_order,
 SUM(m_nb_customer) AS nb_customer,
 
 --turnover
-ROUND(SUM(m_cached_payed),2) AS tunover,
+ROUND(SUM(m_cached_payed),2) AS turnover,
 ROUND(SUM(m_cached_payed)/COUNT(id_order),2) AS avg_turnover_per_order
 
 FROM {{ ref('stg_order_data') }}
